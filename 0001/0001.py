a8ce555b1ea19816bc9d4b2240084f074e6e35f4
lionng <154704230@qq.com>
2019-12-22 22:22:59 +0800

    0001

---
+++ 0001/0001.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+-------------------------------------------------
+   File Name：     0001
+   Author :       zhang
+   date：          19-12-22
+-------------------------------------------------
+"""
+import random
+for i in range(0,200):
+    for j in range(0,4):
+        print(random.randint(0,9),end='')
+    print()
